aabfd722ecf157696664950046c3325f69a999c2
JuditKaramazov <j.lazaromoyano7@gmail.com>
2023-07-21 00:02:54 +0200

    Improve App

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,21 @@
+import { AudioProvider } from './context/AudioContext';
 import { TimerProvider } from './context/TimerContext';
 import { ThemeSwitcher } from './components/client/ThemeSwitcher';
+import Audio from './components/server/Audio';
 import Timer from './components/server/Timer';
 import './App.css';
 
 export default function App() {
-  
+
   return (
     <main>
       <ThemeSwitcher />
       <TimerProvider>
         <Timer />
       </ TimerProvider>
+      <AudioProvider>
+        <Audio />
+      </ AudioProvider>
     </main>
   )
 }
