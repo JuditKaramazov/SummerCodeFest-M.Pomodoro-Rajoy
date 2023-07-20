f3571f3278a98ea2403c7b31e9ecec5a679ee819
JuditKaramazov <j.lazaromoyano7@gmail.com>
2023-07-20 19:58:11 +0200

    Fix ThemeContext

--- src/context/ThemeContext.tsx
+++ src/context/ThemeContext.tsx
@@ -1,4 +1,4 @@
-import React, { createContext, useContext, useState, useEffect } from 'react';
+import React, { createContext, useContext, useEffect, useState } from 'react';
 import { ThemeContextProps } from '../types/context/theme';
 
 const ThemeContext = createContext({} as ThemeContextProps);
@@ -9,28 +9,24 @@ export function ThemeProvider({
   children: React.ReactNode
 }) {
   const [isDarkThemeActive, setIsDarkThemeActive] = useState(false);
-
   const htmlTag = document.querySelector('html')!;
-
-  let dayThemeButton = document.querySelector('div.light');
-  let nightThemeButton = document.querySelector('div.dark');
+  const dayThemeButton = document.querySelector('div.light');
+  const nightThemeButton = document.querySelector('div.dark');
   
   useEffect(() => {
     if (isDarkThemeActive) {
       htmlTag.className = 'dark';
-
       dayThemeButton?.classList.add('hide');
       nightThemeButton?.classList.remove('hide');
     } else {
       htmlTag.className = 'light';
-
       dayThemeButton?.classList.remove('hide');
       nightThemeButton?.classList.add('hide');
     }
   }, [isDarkThemeActive]);
 
   function toggleTheme(){ 
-    let changeTheme = !isDarkThemeActive;
+    const changeTheme = !isDarkThemeActive;
     setIsDarkThemeActive(changeTheme);
     console.log(isDarkThemeActive)
   }
@@ -40,18 +36,16 @@ export function ThemeProvider({
       toggleTheme
     }}>
       {children}
-    </ThemeContext.Provider>
+    </ ThemeContext.Provider>
   )
 }
 
 export function useTheme() {
   const ctx = useContext(ThemeContext);
-
   const hasContext = ctx ?? undefined;
 
   if (!hasContext) {
-    throw new Error("Context not found");
+    throw new Error('Context not found');
   }
-
   return ctx;
 }
