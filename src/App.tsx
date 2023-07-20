41b7c01ab9029bfd93d6e76a3ef5bcb104870750
JuditKaramazov <j.lazaromoyano7@gmail.com>
2023-07-20 20:45:27 +0200

    Add Timer (App.tsx)

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 import { TimerProvider } from './context/TimerContext';
 import { ThemeSwitcher } from './components/client/ThemeSwitcher';
+import Timer from './components/server/Timer';
 import './App.css';
 
 export default function App() {
+  
   return (
     <main>
       <ThemeSwitcher />
       <TimerProvider>
-
+        <Timer />
       </ TimerProvider>
     </main>
   )
