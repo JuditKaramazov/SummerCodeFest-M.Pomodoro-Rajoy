a39421ca599251600684cb32c57282f21950d240
JuditKaramazov <j.lazaromoyano7@gmail.com>
2023-07-22 21:49:44 +0200

    Fix styles and structure

--- src/App.tsx
+++ src/App.tsx
@@ -2,20 +2,29 @@ import { AudioProvider } from './context/AudioContext';
 import { TimerProvider } from './context/TimerContext';
 import { ThemeSwitcher } from './components/client/ThemeSwitcher';
 import Audio from './components/server/Audio';
+import RandomQuoteButton from './components/client/RandomQuoteController';
 import Timer from './components/server/Timer';
 import './App.css';
 
 export default function App() {
-
   return (
     <main>
-      <ThemeSwitcher />
-      <TimerProvider>
-        <Timer />
-      </ TimerProvider>
-      <AudioProvider>
-        <Audio />
-      </ AudioProvider>
+      <div className="left-container">
+        <TimerProvider>
+          <Timer />
+        </ TimerProvider>
+      </div>
+      <div className="right-container">
+        <div className="top-right">
+          <ThemeSwitcher />
+          <RandomQuoteButton />
+        </div>
+        <div className="bottom-right">
+          <AudioProvider>
+            <Audio />
+          </ AudioProvider>
+        </div>
+      </div>
     </main>
-  )
+  );
 }
